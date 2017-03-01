aef177e9bc0d84c1a952a572def9b6f2563c0810
yurkevichS <yurkevich.s@gmail.com>
2017-03-01 03:24:33 -0800

    added main page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
       resources :cities
       resources :states
      end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+     get '/ui' => "ui#index"
+     get '/ui#' => "ui#index"
+
+     root "ui#index"
+
 end
